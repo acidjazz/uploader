b19241453f2725bce46fde28bc537ade93df9e8a
kevin olson <acidjazz@gmail.com>
2018-03-06 04:34:41 -0600

    ✨ proper image selection on our new inventory add page

--- lib/pages/inventoryModify.dart
+++ lib/pages/inventoryModify.dart
@@ -1,11 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class InventoryModify extends StatefulWidget {
   var id = false;
   InventoryModify(this.id);
 
-  String titleNew = 'Adding Inventory';
-  String titleEdit = 'Modifying inventory';
+  final String titleNew = 'Adding Inventory';
+  final String titleEdit = 'Modifying inventory';
 
   @override
   InventoryModifyState createState() => new InventoryModifyState();
@@ -14,14 +17,31 @@ class InventoryModify extends StatefulWidget {
     print('saving new inventory');
   }
 
-  choosePhotos () {
-    print('choosing photos');
-  }
 }
 
 class InventoryModifyState extends State<InventoryModify> {
 
+  List<File> photos = new List();
+
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  choosePhotos () async {
+    var _file = await ImagePicker.pickImage();
+    setState(() {
+      this.photos.add(_file);
+      print('PHOTO PATH');
+      print(this.photos.first.path);
+    });
+  }
+
+  List<Widget> photosWidget () {
+    return this.photos.map((File photo) {
+      return new Container(
+        padding: new EdgeInsets.symmetric(horizontal: 10.0),
+        child: new Image.file(photo, width: 100.0),
+      );
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,13 +63,25 @@ class InventoryModifyState extends State<InventoryModify> {
           child: new Container(
             padding: new EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
             child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
 
                 new RaisedButton(
-                  onPressed: widget.choosePhotos,
+                  onPressed: choosePhotos,
                   child: new Text('Add Photos'),
                   color: Colors.blue,
                   textColor: Colors.white,
+                ),
+
+                new Container(
+                  padding: new EdgeInsets.symmetric(vertical: 20.0),
+                  height: MediaQuery.of(context).size.height*0.2,
+                  child: new ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: photos.length == 0
+                      ? [ new Text('No image selected yet') ]
+                      : photosWidget()
+                  ),
                 ),
 
                 new TextFormField(
