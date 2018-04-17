3d573e1f6cc211e13f1f599886a5c97864bdb2c0
kevin olson <acidjazz@gmail.com>
2018-04-16 20:56:42 -0500

    🔧 more fixes on the inventory page to accomodate stricter types

--- lib/pages/inventory.dart
+++ lib/pages/inventory.dart
@@ -17,7 +17,6 @@ class Inventory extends StatefulWidget {
 }
 
 class InventoryState extends State<Inventory> {
-
   var _loaded = false;
   var _loadedSignal = false;
   var _signalSubscription;
@@ -31,28 +30,34 @@ class InventoryState extends State<Inventory> {
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
-  Future<void> loadInventory () async {
+  Future<void> loadInventory() async {
     await inventory.load(widget.name);
     _bottomBarIndex();
     user.load();
-    setState(() { _loaded = true; });
+    setState(() {
+      _loaded = true;
+    });
   }
 
-  void loadSignal () async {
+  void loadSignal() async {
     var result = await (new Connectivity().checkConnectivity());
     _connection = setConnectivity(result);
-    setState(() { _loadedSignal = true; });
+    setState(() {
+      _loadedSignal = true;
+    });
 
     if (_signalSubscription == null) {
-      _signalSubscription = new Connectivity().onConnectivityChanged
-      .listen((ConnectivityResult result) {
-        setState(() { _connection = setConnectivity(result); });
+      _signalSubscription = new Connectivity()
+          .onConnectivityChanged
+          .listen((ConnectivityResult result) {
+        setState(() {
+          _connection = setConnectivity(result);
+        });
       });
     }
-
   }
 
-  IconData setConnectivity (ConnectivityResult result) {
+  IconData setConnectivity(ConnectivityResult result) {
     if (result == ConnectivityResult.mobile) {
       _internet = true;
       return Icons.signal_cellular_4_bar;
@@ -65,41 +70,37 @@ class InventoryState extends State<Inventory> {
     }
   }
 
-  Widget _body () {
+  Widget _body() {
     if (_loaded) {
       if (inventory.items.length < 1) {
         return new Center(
-          child: new Text('Click the "+" to add an item to ${widget.name}', style: new TextStyle(fontSize: 20.0),));
+            child: new Text(
+          'Click the "+" to add an item to ${widget.name}',
+          style: new TextStyle(fontSize: 20.0),
+        ));
       } else {
         return _inventoryWidget();
       }
     } else {
-      return new Center(
-        child: new CircularProgressIndicator()
-      );
+      return new Center(child: new CircularProgressIndicator());
     }
   }
 
-  Widget _inventoryWidget () {
+  Widget _inventoryWidget() {
     return new ListView(
-      children: inventory.items.map((item)
-        => _listProgressTile(item)).toList(),
+      children: inventory.items.map((item) => _listProgressTile(item)).toList(),
     );
   }
 
   Widget _listProgressTile(item) {
     double _progress = item.progress;
-    return new Stack(
-      alignment: Alignment.bottomCenter,
-      children: <Widget>[
-        _listTile(item),
-        new LinearProgressIndicator(value: _progress),
-      ]
-    );
+    return new Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+      _listTile(item),
+      new LinearProgressIndicator(value: _progress),
+    ]);
   }
 
   Widget _leading(item) {
-
     if (item.uploading == 'true') {
       return new CircularProgressIndicator();
     }
@@ -111,11 +112,13 @@ class InventoryState extends State<Inventory> {
     return new Icon(Icons.file_upload);
   }
 
-  Widget _listTile(item) {
+  Widget _listTile(InventoryItem item) {
     return new ListTile(
       title: new Text(item.name),
       subtitle: new Text(item.description),
-      onTap: () { _toInventoryModify(inventory.items.indexOf(item), item); },
+      onTap: () {
+        _toInventoryModify(inventory.items.indexOf(item), item);
+      },
       isThreeLine: true,
       leading: _leading(item),
       trailing: new Container(
@@ -124,59 +127,56 @@ class InventoryState extends State<Inventory> {
         child: new Stack(
           alignment: Alignment.center,
           overflow: Overflow.visible,
-          children: item.photos.getRange(0,
-            item.photos.length > 2 ? 3 : item.photos.length)
-              .toList().reversed.map((photo) =>
-            new Positioned(
-              right: 20.0*item.photos.indexOf(photo),
-              width: 60.0,
-              height: 60.0,
-              child: new Container(
-                child: new Image.file(
-                  new File(
-                    inventory.path(photo.path)),
-                  fit: BoxFit.cover),
-                decoration: new BoxDecoration(
-                  border: new Border(right: new BorderSide(
-                    width: photo == item.photos.first ? 0.0 : 1.0,
-                    color: Colors.white)
-                  ),
-                ),
-              ),
-            ),
-          ).toList(),
+          children: item.photos
+              .getRange(0, item.photos.length > 2 ? 3 : item.photos.length)
+              .toList()
+              .reversed
+              .map(
+                (photo) => new Positioned(
+                      right: 20.0 * item.photos.indexOf(photo),
+                      width: 60.0,
+                      height: 60.0,
+                      child: new Container(
+                        child: new Image.file(
+                            new File(inventory.path(photo.path)),
+                            fit: BoxFit.cover),
+                        decoration: new BoxDecoration(
+                          border: new Border(
+                              right: new BorderSide(
+                                  width: photo == item.photos.first ? 0.0 : 1.0,
+                                  color: Colors.white)),
+                        ),
+                      ),
+                    ),
+              )
+              .toList(),
         ),
       ),
     );
   }
 
-  Future<Null> _toInventoryModify (int index, item) async {
-    await Navigator.of(context).push(
-      new MaterialPageRoute(
+  Future<Null> _toInventoryModify(int index, InventoryItem item) async {
+    await Navigator.of(context).push(new MaterialPageRoute(
         fullscreenDialog: true,
-        builder: (context) => new InventoryModify(index, item, widget.name)
-      )
-    );
+        builder: (context) => new InventoryModify(index, item, widget.name)));
     _loaded = false;
   }
 
-  Future<Null> _toInventoryCreate () async {
-    await Navigator.of(context).push(
-      new MaterialPageRoute(
-        fullscreenDialog: true,
-        builder: (context) => new InventoryModify(null, null, widget.name),
-      )
-    );
+  Future<Null> _toInventoryCreate() async {
+    await Navigator.of(context).push(new MaterialPageRoute(
+          fullscreenDialog: true,
+          builder: (context) => new InventoryModify(null, null, widget.name),
+        ));
     _loaded = false;
   }
+
   void _snackBar(message) {
     _scaffoldKey.currentState.showSnackBar(new SnackBar(
       content: new Text(message),
     ));
   }
 
-  String _canAction () {
-
+  String _canAction() {
     if (inventory.items == null) {
       return 'Inventory not loaded yet';
     }
@@ -192,52 +192,50 @@ class InventoryState extends State<Inventory> {
     return null;
   }
 
-
-  void _cancelUpload () {
+  void _cancelUpload() {
     inventory.cancel = 'true';
   }
 
-  String _validateWorkspaceId (String value) {
+  String _validateWorkspaceId(String value) {
     if (value.isEmpty) {
       return 'Please specify a workspace';
     }
     return null;
   }
 
-  void _publishDialog () {
+  void _publishDialog() {
     showDialog(
       context: context,
-      child: new _SystemPadding(child: new AlertDialog(
-        title: new Text("Workspace ID"),
-        content: new Form(
-          key: _formKey,
-          child: new TextFormField(
-            decoration: new InputDecoration(
-              labelText: '${user.email}-#',
+          child: new AlertDialog(
+              title: new Text("Workspace ID"),
+              content: new Form(
+                key: _formKey,
+                child: new TextFormField(
+                  decoration: new InputDecoration(
+                    labelText: '${user.email}-#',
+                  ),
+                  keyboardType: TextInputType.number,
+                  onSaved: (String value) {
+                    workspaceId = value;
+                  },
+                  validator: _validateWorkspaceId,
+                  autofocus: true,
+                ),
+              ),
+              actions: [
+            new FlatButton(
+              child: new Text("CANCEL"),
+              onPressed: () => Navigator.pop(context),
             ),
-            keyboardType: TextInputType.number,
-            onSaved: (String value) { workspaceId = value; },
-            validator: _validateWorkspaceId,
-            autofocus: true,
-
-          ),
-        ),
-        actions: [
-          new FlatButton(
-            child: new Text("CANCEL"),
-            onPressed: () => Navigator.pop(context),
-          ),
-          new FlatButton(
-            child: new Text("PUBLISH"),
-            onPressed: _publishItems,
-          ),
-        ]
-      )),
+            new FlatButton(
+              child: new Text("PUBLISH"),
+              onPressed: _publishItems,
+            ),
+          ]),
     );
   }
 
-  Future<bool> _publishItems () async {
-
+  Future<bool> _publishItems() async {
     final FormState form = _formKey.currentState;
     form.save();
 
@@ -252,10 +250,10 @@ class InventoryState extends State<Inventory> {
     _bottomBarIndex();
     inventory.save(widget.name);
     setState(() => _snackBar('Inventory successfully published'));
-
+    return true;
   }
-  Future<bool> _uploadItems () async {
 
+  Future<bool> _uploadItems() async {
     if (_canAction() != null) {
       _snackBar(_canAction());
       return false;
@@ -267,7 +265,6 @@ class InventoryState extends State<Inventory> {
     });
 
     for (var itemIndex = 0; itemIndex < inventory.items.length; itemIndex++) {
-
       if (inventory.cancel == 'true') {
         break;
       }
@@ -284,7 +281,6 @@ class InventoryState extends State<Inventory> {
       });
 
       for (var photoIndex = 0; photoIndex < item.photos.length; photoIndex++) {
-
         if (inventory.cancel == 'true') {
           break;
         }
@@ -293,7 +289,7 @@ class InventoryState extends State<Inventory> {
 
         await photo.upload('${itemIndex+1}-${photoIndex+1}');
         setState(() {
-          item.progress = (photoIndex+1)/item.photos.length;
+          item.progress = (photoIndex + 1) / item.photos.length;
         });
       }
 
@@ -304,7 +300,6 @@ class InventoryState extends State<Inventory> {
         inventory.save(widget.name);
         _snackBar('Images for ${item.name} saved');
       });
-
     }
 
     if (inventory.cancel == 'true') {
@@ -322,11 +317,9 @@ class InventoryState extends State<Inventory> {
     });
 
     return true;
-
   }
 
-  int _bottomBarIndex () {
-
+  int _bottomBarIndex() {
     if (inventory.published == 'true') {
       setState(() => _bottomIndex = 2);
       return 2;
@@ -341,8 +334,7 @@ class InventoryState extends State<Inventory> {
     return 0;
   }
 
-
-  void _bottomBar (choice) {
+  void _bottomBar(choice) {
     if (choice == 0) {
       if (inventory.uploading == 'true') {
         _cancelUpload();
@@ -362,41 +354,39 @@ class InventoryState extends State<Inventory> {
       // we need to prompt them to make sure
       _removeDialog();
     }
-
   }
 
-  void _removeDialog () {
+  void _removeDialog() {
     showDialog(
       context: context,
       child: new AlertDialog(
-        title: new Text('Confirmation'),
-        content: new Text('Are you sure you want to delete "${widget.name}?"'),
-        actions: [
-          new FlatButton(
-            child: new Text("CANCEL"),
-            onPressed: () => Navigator.pop(context),
-          ),
-          new FlatButton(
-            child: new Text("DELETE"),
-            onPressed: _removeInventory,
-          ),
-        ]
-      ),
+          title: new Text('Confirmation'),
+          content:
+              new Text('Are you sure you want to delete "${widget.name}?"'),
+          actions: [
+            new FlatButton(
+              child: new Text("CANCEL"),
+              onPressed: () => Navigator.pop(context),
+            ),
+            new FlatButton(
+              child: new Text("DELETE"),
+              onPressed: _removeInventory,
+            ),
+          ]),
     );
   }
 
-  Future<Null> _removeInventory () async {
+  Future<Null> _removeInventory() async {
     Navigator.pop(context);
     await inventories.remove(widget.name);
     Navigator.pop(context);
     _snackBar('Inventory Removed');
   }
 
-  Future<bool> _onWillPop () async => inventory.uploading == 'false';
+  Future<bool> _onWillPop() async => inventory.uploading == 'false';
 
   @override
   Widget build(BuildContext context) {
-
     if (!_loaded) loadInventory();
     if (!_loadedSignal) loadSignal();
 
@@ -404,12 +394,9 @@ class InventoryState extends State<Inventory> {
       onWillPop: _onWillPop,
       child: new Scaffold(
         key: _scaffoldKey,
-        appBar: new AppBar(
-          title: new Text(widget.name),
-          actions: [
-            new Icon(_connection),
-          ]
-        ),
+        appBar: new AppBar(title: new Text(widget.name), actions: [
+          new Icon(_connection),
+        ]),
         body: _body(),
         floatingActionButton: new FloatingActionButton(
           tooltip: 'Add',
@@ -417,44 +404,27 @@ class InventoryState extends State<Inventory> {
           onPressed: _toInventoryCreate,
         ),
         bottomNavigationBar: new BottomNavigationBar(
-          onTap: _bottomBar,
-          currentIndex: _bottomIndex,
-          items: [
-            new BottomNavigationBarItem(
-              icon: new Icon(
-                inventory.uploading == 'true' ? Icons.cancel : Icons.file_upload),
-              title: new Text(
-                inventory.uploading == 'true' ? 'Cancel Upload' : 'Upload Photos'),
-            ),
-            new BottomNavigationBarItem(
-              icon: new Icon(Icons.cloud_upload),
-              title: new Text('Publish Inventory'),
-            ),
-            new BottomNavigationBarItem(
-              icon: new Icon(Icons.delete_forever),
-              title: new Text('Remove Inventory'),
-            ),
-          ]
-        ),
+            onTap: _bottomBar,
+            currentIndex: _bottomIndex,
+            items: [
+              new BottomNavigationBarItem(
+                icon: new Icon(inventory.uploading == 'true'
+                    ? Icons.cancel
+                    : Icons.file_upload),
+                title: new Text(inventory.uploading == 'true'
+                    ? 'Cancel Upload'
+                    : 'Upload Photos'),
+              ),
+              new BottomNavigationBarItem(
+                icon: new Icon(Icons.cloud_upload),
+                title: new Text('Publish Inventory'),
+              ),
+              new BottomNavigationBarItem(
+                icon: new Icon(Icons.delete_forever),
+                title: new Text('Remove Inventory'),
+              ),
+            ]),
       ),
     );
-  }
-
-}
-
-// remove this after beta update, shouldn't be needed anymore
-// ref: https://github.com/flutter/flutter/pull/15426
-class _SystemPadding extends StatelessWidget {
-  final Widget child;
-
-  _SystemPadding({Key key, this.child}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var mediaQuery = MediaQuery.of(context);
-    return new AnimatedContainer(
-      padding: mediaQuery.viewInsets,
-      duration: const Duration(milliseconds: 60),
-      child: child);
   }
 }
